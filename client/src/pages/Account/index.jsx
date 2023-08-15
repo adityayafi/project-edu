6935eea2c1d52231a53fa4a044a8d59012056a56
aditya yafi <adityayafi@gmail.com>
2023-08-15 22:49:27 +0700

    adding admin menu

--- client/src/pages/Account/index.jsx
+++ client/src/pages/Account/index.jsx
@@ -1,30 +1,43 @@
 // import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCartPlus, faClipboardList, faHome, faLocationDot, faRightFromBracket} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBoxOpen, faCartPlus, faClipboardList, faFileInvoice, faHome, faList, faLocationDot, faRightFromBracket, faTags, faUserGear} from '@fortawesome/free-solid-svg-icons'
 import { Outlet, useNavigate } from 'react-router-dom';
 import { logoutUser } from '../../api/auth';
+import { currentUser } from '../../utils';
 
 const Account = () => {
 
+    const {user} = currentUser ? JSON.parse(currentUser) : null;
     const navigate = useNavigate();
 
-    function getItem(label, key, icon) {
+    function getItem(label, key, icon, children) {
         return {
           key,
           icon,
           label,
+          children,
         };
     }
 
     const items = [
-        getItem('Profil', 'profile', <FontAwesomeIcon icon={faCartPlus} className="text-white"/>),
-        getItem('Pemesanan', 'order', <FontAwesomeIcon icon={faClipboardList} className="text-white"/>),
-        getItem('Alamat', 'address', <FontAwesomeIcon icon={faLocationDot} className="text-white"/>),
+        getItem('Profil', '/account', <FontAwesomeIcon icon={faCartPlus} className="text-white"/>),
+        getItem('Pemesanan', '/account/order', <FontAwesomeIcon icon={faClipboardList} className="text-white"/>),
+        getItem('Alamat', '/account/address', <FontAwesomeIcon icon={faLocationDot} className="text-white"/>),
         getItem('Home', '/', <FontAwesomeIcon icon={faHome} className="text-white"/>),
         getItem('Log Out', 'logout', <FontAwesomeIcon icon={faRightFromBracket} className="text-white"/>),
 
     ];
+
+    if (user.role === 'admin') {
+        items.splice(4, 0, getItem('Menu Admin', 'sub1', <FontAwesomeIcon icon={faUserGear} className="text-white"/>, [
+            getItem('Products', '/account/admin/products', <FontAwesomeIcon icon={faBoxOpen}/>),
+            getItem('Categories', '/account/admin/categories', <FontAwesomeIcon icon={faList}/>),
+            getItem('Tags', '/account/admin/tags', <FontAwesomeIcon icon={faTags}/>),
+            getItem('Invoices', '/account/admin/invoice', <FontAwesomeIcon icon={faFileInvoice}/>),
+          ])
+        )
+    }
 
     const onClick = ({key}) => {
         if (key === 'logout'){
@@ -34,6 +47,8 @@ const Account = () => {
         }
       };
 
+      console.log(window.location.pathname)
+
     return (
         <div className='flex'>
             <Menu
@@ -41,8 +56,9 @@ const Account = () => {
             style={{
                 width: 256,
             }}
-            defaultSelectedKeys={['profile']}
-            defaultOpenKeys={['profile']}
+            selectedKeys={window.location.pathname}
+            defaultSelectedKeys={['/account']}
+            defaultOpenKeys={['/account']}
             mode="inline"
             items={items}
             className='min-h-screen'
