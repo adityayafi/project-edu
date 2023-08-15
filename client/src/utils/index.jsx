9809a2b458a1f02eacc7ca5d2ee7d23879520a5f
aditya yafi <adityayafi@gmail.com>
2023-08-15 22:48:55 +0700

    create a fucntion to convert date format

--- client/src/utils/index.jsx
+++ client/src/utils/index.jsx
@@ -4,4 +4,24 @@ export const Rupiah = (value) => {
     return "Rp." + res; // add prefix 'Rp.' to the result string for better
 }
 
+export const ISOtoObj = (isodate) => {
+    let dateObj = new Date(isodate);
+    let year = dateObj.getFullYear();
+    let month = dateObj.getMonth()+1;
+    let day = dateObj.getDay();
+    let hr = dateObj.getHours();
+    let min = dateObj.getMinutes();
+    let sec = dateObj.getSeconds();
+
+    if(day < 10){
+        day = '0'+day;
+    }
+    if(month < 10){
+        month = '0' + month;
+    }
+    let newdate = `${day}-${month}-${year} ${hr}:${min}:${sec}`;
+
+    return newdate;
+}
+
 export const currentUser = localStorage.getItem('auth')
