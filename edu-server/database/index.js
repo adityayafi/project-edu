41f7c275c7a6c3127e02760131998de169eb2702
aditya yafi <adityayafi@gmail.com>
2023-08-15 21:06:49 +0700

    update database config 2

--- edu-server/database/index.js
+++ edu-server/database/index.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const {dbHost, dbPass, dbName, dbPort, dbUser} = require('../app/config');
 mongoose.set("strictQuery", false);
 try {
-    mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+    // mongoose.connect(`mongodb://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
+    mongoose.connect(`mongodb+srv://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}?authSource=admin`);
     // mongoose.connect(`mongodb+srv://adityayafi:48wQ8aCUa2cpFuQQ@cluster0.0n0vm3g.mongodb.net/?retryWrites=true&w=majority`);
     console.log("MongoDB Connection Success")
 } catch (error) {
