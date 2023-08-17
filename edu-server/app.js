efb990c28feeea3b2ab22c33061d1d0c87fe44e9
aditya yafi <adityayafi@gmail.com>
2023-08-17 23:43:13 +0700

    fix image display

--- edu-server/app.js
+++ edu-server/app.js
@@ -38,7 +38,7 @@ app.use('/api', cartRoute);
 app.use('/api', orderRoute);
 app.use('/api', invoiceRoute);
 // app.use('/public', express.static(path.join(__dirname, '/public/images/products')));
-app.use('/public', express.static('/public/images/products'));
+app.use('/public', express.static('public/images/products'));
 
 app.use('/', (req, res) => {
   res.render('index', {
