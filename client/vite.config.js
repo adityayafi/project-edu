edd6e7d0e99353733656dfaa538ecee39bf763da
aditya yafi <adityayafi@gmail.com>
2023-08-17 16:27:40 +0700

    fix route error

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-        external: ["react", "react-router", "react-router-dom", "react-redux"],
-        output:{
-            manualChunks(id) {
-                if (id.includes('node_modules')) {
-                    return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                }
-            }
-        }
-    }
-  }
+  // build: {
+  //   rollupOptions: {
+  //       output:{
+  //           manualChunks(id) {
+  //               if (id.includes('node_modules')) {
+  //                   return id.toString().split('node_modules/')[1].split('/')[0].toString();
+  //               }
+  //           }
+  //       }
+  //   }
+  // }
 })
